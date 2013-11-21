db9db45621e1fb03c5b31a5628bd8da4c0218ca2
Paul Irish <paul.irish@gmail.com>
2013-11-21 10:03:09 -0800

    update base.js

--- js/base.js
+++ js/base.js
@@ -1,45 +1,61 @@
-var hasTouch              = Modernizr.touch,
-	hasMotion             = Modernizr.devicemotion,
-	hasOrientation        = Modernizr.deviceorientation,
-	has3d                 = Modernizr.csstransforms3d,
-	foldingEvent          = 'pageFolding',
-	rotationEvent         = (hasTouch && hasOrientation) ? 'deviceorientation' : 'mousemove',
-	dragStartEvent        = hasTouch ? 'touchstart' : 'mousedown',
-	dragMoveEvent         = hasTouch ? 'touchmove' : 'mousemove',
-	dragStopEvent         = hasTouch ? 'touchend' : 'mouseup',
-	selectionEvent        = hasTouch ? 'touchend' : 'click',
-	domTransformProperty  = Modernizr.prefixed('transform'),
-	cssTransformProperty  = domToCss(domTransformProperty),
-	domTransitionProperty = Modernizr.prefixed('transition'),
-	cssTransitionProperty = domToCss(domTransitionProperty),
-	transitionEndEvent    = {
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 0:35:11 May 13, 2013 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 18:00:07 Nov 21, 2013.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+var popups,
+	paperFolding = 'pageFolding',
+	curPageIndex,
+	timer,
+	zoomedIn,
+	curSceneScale,
+	curRotX,
+	curRotY,
+	curPer,
+	curDir,
+	adjustedPer,
+	bgColors,
+	domTransformProperty,
+	cssTransformProperty,
+	domTransitionProperty,
+	cssTransitionProperty,
+	$leftPage,
+	$rightPage,
+	$document = $(document),
+	$window = $(window),
+	$body,
+	$scene,
+	$book,
+	$spreads,
+	$dragNotice,
+	$hotSpots,
+	hasTouch,
+	hasMotion,
+	hasOrientation,
+	numTouches,
+	rotationEvent,
+	dragStartEvent,
+	dragMoveEvent,
+	dragStopEvent,
+	selectionEvent,
+	transitionEndEvents = {
 		'WebkitTransition' : 'webkitTransitionEnd',
 		'MozTransition'    : 'transitionend',
 		'OTransition'      : 'oTransitionEnd',
 		'msTransition'     : 'MSTransitionEnd',
 		'transition'       : 'transitionend'
-	}[ domTransitionProperty ],
-	PAGE_TURN_SPEED       = 25,
-	zoomedIn              = false,
-	curPageIndex          = 0,
-	curSceneScale         = 1,
-	curRotX               = -15,
-	curRotY               = 0,
-	curPer                = 0,
-	adjustedPer           = 0,
-	numTouches            = 0,
-	bgColors              = ['#3272ad', '#ae0039', '#50326d', '#355506', '#3272ad'],
-	$document             = $(document),
-	$window               = $(window),
-	$body,
-	$scene,
-	$book,
-	$spreads,
-	$leftPage,
-	$rightPage,
-	$dragNotice,
-	$hotSpots
-;
+	},
+	transitionEndEvent,
+	initialOrientation,
+	PAGE_TURN_SPEED = 25;
 
 
 
@@ -56,12 +72,35 @@ var hasTouch              = Modernizr.touch,
 
 $document.ready(function () {
 
-	$body       = $('body');
-	$scene      = $('.scene');
-	$book       = $('.book');
-	$spreads    = $('.spread');
+	curPageIndex = 0;
+	curSceneScale = 1;
+	curRotX = -15;
+	curRotY = 0;
+	curPer = adjustedPer = 0;
+	zoomedIn = false;
+	curRotX = -15;
+	numTouches = 0;
+	domTransformProperty = Modernizr.prefixed('transform');
+	cssTransformProperty = domToCss(domTransformProperty);
+	domTransitionProperty = Modernizr.prefixed('transition');
+	cssTransitionProperty = domToCss(domTransitionProperty);
+	bgColors = ['#3272ad', '#ae0039', '#50326d', '#355506', '#3272ad'];
+	$body = $('body');
+	$scene = $('.scene');
+	$book = $('.book');
+	$spreads = $('.spread');
 	$dragNotice = $('.drag-notice');
-	$hotSpots   = $('.hotspot');
+	$hotSpots = $('.hotspot');
+	hasTouch = Modernizr.touch;
+	hasMotion = Modernizr.devicemotion;
+	hasOrientation = Modernizr.deviceorientation;
+	rotationEvent = (hasTouch && hasOrientation) ? 'deviceorientation' : 'mousemove';
+	dragStartEvent = hasTouch ? 'touchstart' : 'mousedown';
+	dragMoveEvent = hasTouch ? 'touchmove' : 'mousemove';
+	dragStopEvent = hasTouch ? 'touchend' : 'mouseup';
+	selectionEvent = hasTouch ? 'touchend' : 'click';
+	transitionEndEvent = transitionEndEvents[ domTransitionProperty ];
+	has3d = Modernizr.csstransforms3d;
 
 	if (has3d) {
 		craftThatPaperBaby();
@@ -72,17 +111,10 @@ $document.ready(function () {
 				'width': '220px'
 			});
 		}
-
 		$window.bind(rotationEvent, rotateScene);
 		$window.bind('resize', resizeScene);
 		$body.bind(dragStartEvent, startDrag);
-		$body.on(selectionEvent, $hotSpots.selector, onHotspotSelected);
-		// Sometimes, when zoomed in, the click target is `body`; not a hotspot
-		$body.on(selectionEvent, function (e) {
-			if (zoomedIn && e.target == e.currentTarget) {
-				onHotspotSelected(e);
-			}
-		});
+		$hotSpots.bind(selectionEvent, zoomToHotspot);
 
 		resizeScene();
 		adjustScene();
@@ -103,37 +135,35 @@ $document.ready(function () {
 ****************************/
 
 function startDrag(e) {
-	if (zoomedIn) return;
 	e.preventDefault();
 	$book.css('cursor', '-webkit-grabbing');
-	var data = {
-		offset: hasTouch ? e.originalEvent.touches[0].pageX : e.pageX,
-		width: $body.width()
-	};
-
-	$body.bind(dragMoveEvent, data, updateDrag);
+	$scene.data('offset', hasTouch ? e.originalEvent.touches[0].pageX : e.pageX);
+	$body.bind(dragMoveEvent, updateDrag);
 	$body.bind(dragStopEvent, stopDrag);
 }
 
 function updateDrag(e) {
 	e.preventDefault();
 
-	$dragNotice.removeClass('shown');
+	if ($dragNotice.hasClass('shown')) {
+		$dragNotice.removeClass('shown');
+	}
 
-	var offset      = e.data.offset;
-	var width       = e.data.width;
-	var targetX     = hasTouch ? e.originalEvent.touches[0].pageX : e.pageX - offset;
-	var per         = clamp(targetX / width * 2.5, -1, 1);
+	var targetX = (hasTouch ? e.originalEvent.touches[0].pageX : e.pageX) - $scene.data('offset');
+	var per = targetX / $body.width() * 2.5;
 	var shouldBreak = false;
-	var dir         = per < 0 ? 'left' : 'right';
+
+	per = clamp(per, -1, 1);
+
+	var dir = per < 0 ? 'left' : 'right';
 
 	if (curPer + per < 0 && adjustedPer > 0) {
 		shouldBreak = true;
-		curPer      = adjustedPer = 0;
+		curPer = adjustedPer = 0;
 		stopDrag();
 	} else if (curPer + per > 0 && adjustedPer < 0) {
 		shouldBreak = true;
-		curPer      = adjustedPer = 0;
+		curPer = adjustedPer = 0;
 		stopDrag();
 	} else {
 		adjustedPer = clamp(curPer + per, -1, 1);
@@ -159,60 +189,51 @@ function updateDrag(e) {
 	var leftTransform;
 	var rightTransform;
 	if (!shouldBreak) {
-		$leftPage      = adjustedPer < 0 ? $spreads.eq( curPageIndex ) : $spreads.eq( curPageIndex - 1 );
-		$rightPage     = adjustedPer < 0 ? $spreads.eq(curPageIndex + 1) : $spreads.eq(curPageIndex);
+		$leftPage = adjustedPer < 0 ? $spreads.eq( curPageIndex ) : $spreads.eq( curPageIndex - 1 );
+		$rightPage = adjustedPer < 0 ? $spreads.eq(curPageIndex + 1) : $spreads.eq(curPageIndex);
 		rightTransform = 'rotateY(' + (-180 * (adjustedPer < 0 ? absPer : 1 - absPer)) + 'deg)';
-		leftTransform  = 'rotateY(' + (180 * (adjustedPer < 0 ? 1 - absPer : absPer)) + 'deg)';
+		leftTransform = 'rotateY(' + (180 * (adjustedPer < 0 ? 1 - absPer : absPer)) + 'deg)';
 		$leftPage.find('.page-right').css(cssTransformProperty, rightTransform);
 		$rightPage.find('.page-left').css(cssTransformProperty, leftTransform);
 
-		$leftPage.trigger(foldingEvent, adjustedPer < 0 ? absPer : 1 - absPer);
-		$rightPage.trigger(foldingEvent, adjustedPer < 0 ? 1 - absPer : absPer);
+		$leftPage.trigger(paperFolding, adjustedPer < 0 ? absPer : 1 - absPer);
+		$rightPage.trigger(paperFolding, adjustedPer < 0 ? 1 - absPer : absPer);
 
 		toggleVisibles(absPer * 180, adjustedPer < 0 ? curPageIndex : curPageIndex - 1);
 	} else {
 		rightTransform = 'rotateY(' + (-180 * (dir == 'right' ? absPer : 1 - absPer)) + 'deg)';
-		leftTransform  = 'rotateY(' + (180 * (dir == 'right' ? 1 - absPer : absPer)) + 'deg)';
+		leftTransform = 'rotateY(' + (180 * (dir == 'right' ? 1 - absPer : absPer)) + 'deg)';
 		$leftPage.find('.page-right').css(cssTransformProperty, rightTransform);
 		$rightPage.find('.page-left').css(cssTransformProperty, leftTransform);
 
-		$leftPage.trigger(foldingEvent, dir == 'right' ? absPer : 1 - absPer);
-		$rightPage.trigger(foldingEvent, dir == 'right' ? 1 - absPer : absPer);
+		$leftPage.trigger(paperFolding, dir == 'right' ? absPer : 1 - absPer);
+		$rightPage.trigger(paperFolding, dir == 'right' ? 1 - absPer : absPer);
 
 		toggleVisibles(absPer * 180, dir == 'right' ? curPageIndex : curPageIndex - 1);
 	}
 
 	$spreads.each(function (i) {
 		var anglePerPage = 180 / 9;
-		var offsetIndex  = adjustedPer < 0 ? 5 + curPageIndex - i : 5 + curPageIndex - i - 2;
-		var offsetAngle  = adjustedPer < 0 ? offsetIndex - adjustedPer - 1 : offsetIndex - adjustedPer + 1;
-		var rads         = degToRad(offsetAngle * anglePerPage + 10);
-		var tarX         = 5 * Math.cos(rads);
-		var tarZ         = 5 * Math.sin(rads);
-		var transform    = 'translateX(' + tarX.toFixed(3) + 'px) translateZ(' + tarZ.toFixed(3) + 'px) rotateY(' + (i * 0.5) + 'deg)';
+		var offsetIndex = adjustedPer < 0 ? 5 + curPageIndex - i : 5 + curPageIndex - i - 2;
+		var offsetAngle = adjustedPer < 0 ? offsetIndex - adjustedPer - 1 : offsetIndex - adjustedPer + 1;
+		var rads = degToRad(offsetAngle * anglePerPage + 10);
+		var tarX = 5 * Math.cos(rads);
+		var tarZ = 5 * Math.sin(rads);
+		var transform = 'translateX(' + tarX.toFixed(3) + 'px) translateZ(' + tarZ.toFixed(3) + 'px) rotateY(' + (i * 0.5) + 'deg)';
 		$(this).css(cssTransformProperty, transform);
 	});
 
-	var rotateTransform;
-	var translateTransform;
+	var bookTransform;
 	if (curPageIndex == 0) {
-		rotateTransform = 'rotateX(' + (29 + (61 * absPer)) + 'deg) rotateZ(' + (-8 + (8 * absPer)) + 'deg)';
-		translateTransform = 'translate3d(' + (-300 + (300 * absPer)) + 'px, 0, ' + (100 - (100 * absPer)) + 'px)';
+		bookTransform = 'translateX(' + (-300 + (300 * absPer)) + 'px) rotateX(' + (29 + (61 * absPer)) + 'deg) rotateZ(' + (-8 + (8 * absPer)) + 'deg) translateZ(' + (100 - (100 * absPer)) + 'px)';
 	} else if (curPageIndex == 1 && adjustedPer > 0) {
-		rotateTransform = 'rotateX(' + (90 - (61 * absPer)) + 'deg) rotateZ(' + (-8 * absPer) + 'deg)';
-		translateTransform = 'translate3d(' + (-300 * absPer) + 'px, 0, ' + (100 * absPer) + 'px)';
+		bookTransform = 'translateX(' + (-300 * absPer) + 'px) rotateX(' + (90 - (61 * absPer)) + 'deg) rotateZ(' + (-8 * absPer) + 'deg) translateZ(' + (100 * absPer) + 'px)';
 	} else if (curPageIndex == 3 && adjustedPer < 0) {
-		rotateTransform = 'rotateX(' + (90 - (61 * absPer)) + 'deg) rotateZ(' + (-8 * absPer) + 'deg)';
-		translateTransform = 'translate3d(' + (300 * absPer) + 'px, ' + (-120 * absPer) + 'px, ' + (100 * absPer) + 'px)';
+		bookTransform = 'translateX(' + (300 * absPer) + 'px) rotateX(' + (90 - (61 * absPer)) + 'deg) rotateZ(' + (-8 * absPer) + 'deg) translateZ(' + (100 * absPer) + 'px) translateY(' + (-120 * absPer) + 'px)';
 	} else if (curPageIndex == 4) {
-		rotateTransform = 'rotateX(' + (29 + (61 * adjustedPer)) + 'deg) rotateZ(' + (-8 + (8 * adjustedPer)) + 'deg)';
-		translateTransform = 'translate3d(' + (300 - (300 * adjustedPer)) + 'px, ' + (-120 + (120 * adjustedPer)) + 'px, ' + (100 - (100 * adjustedPer)) + 'px)';
+		bookTransform = 'translateX(' + (300 - (300 * adjustedPer)) + 'px) rotateX(' + (29 + (61 * adjustedPer)) + 'deg) rotateZ(' + (-8 + (8 * adjustedPer)) + 'deg) translateZ(' + (100 - (100 * adjustedPer)) + 'px) translateY(' + (-120 + (120 * adjustedPer)) + 'px)';
 	}
-
-	if (translateTransform && rotateTransform) {
-		var bookTransform = translateTransform + ' ' + rotateTransform;
-		$book.css(cssTransformProperty, bookTransform);
-	}
+	$book.css(cssTransformProperty, bookTransform);
 
 	if (absPer >= 1) {
 		adjustedPer < 0 ? curPageIndex++ : curPageIndex--;
@@ -224,13 +245,13 @@ function updateDrag(e) {
 function stopDrag(e) {
 	curDir = null;
 	curPer = adjustedPer;
-	$body.unbind(dragMoveEvent, updateDrag);
-	$body.unbind(dragStopEvent, stopDrag);
+	$body.unbind(dragMoveEvent);
+	$body.unbind(dragStopEvent);
 	$book.css('cursor', '-webkit-grab');
 }
 
 function toggleVisibles(per, leftIndex) {
-	var curPage  = $spreads.eq( curPageIndex );
+	var curPage = $spreads.eq( curPageIndex );
 	var nextPage = $spreads.eq(curPageIndex + 1);
 	var prevPage = $spreads.eq(curPageIndex - 1);
 
@@ -287,27 +308,32 @@ function toggleVisibles(per, leftIndex) {
 
 ****************************/
 
-function onZoomOut() {
-	$scene.css(cssTransitionProperty, 'none');
-	zoomedIn = false;
-}
 
-function onHotspotSelected(e) {
-
+function zoomToHotspot(e) {
 	e.preventDefault();
 
-	var $spot        = $(this);
+	$body.unbind(selectionEvent);
+	$window.unbind(rotationEvent);
+	var $spot = $(this);
 	var $focusedSpot = $hotSpots.filter('.focused');
-	var hotspot      = $focusedSpot.length > 0 ? $focusedSpot : $spot;
-	var indicator    = hotspot.children('.indicator');
+	var hotspot = $focusedSpot.length > 0 ? $focusedSpot : $spot;
+	var indicator = hotspot.children('.indicator');
 
 	if (zoomedIn) {
-
-		$scene.bind(transitionEndEvent, onZoomOut);
-		hotspot.removeClass('focused');
+		setTimeout(function (e) {
+			hotspot.removeClass('focused');
+		}, 1);
 		$spreads.show();
+
 		adjustScene();
 
+		$body.bind(dragStartEvent, startDrag);
+
+		setTimeout(function (e) {
+			$scene.css(cssTransitionProperty, 'none');
+			$scene.unbind(domTransitionProperty);
+			$window.bind(rotationEvent, rotateScene);
+		}, 600);
 	} else {
 		hotspot.addClass('focused');
 		var thisSpread = hotspot.parents('.spread');
@@ -317,27 +343,33 @@ function onHotspotSelected(e) {
 			'margin-top': indicator.attr('data-offsetY') + 'px'
 		});
 
-		$scene.unbind(transitionEndEvent, onZoomOut);
+		$body.unbind(dragStartEvent);
+		$window.unbind(rotationEvent, rotateScene);
+		$body.unbind(dragMoveEvent);
+
 		$scene.css(cssTransitionProperty, 'all .6s');
-		zoomedIn = true;
 
-		var $spread         = $spot.parents('.spread');
-		var scale           = (1 - ((1 - curSceneScale) * 0.3));
-		var scaleTransform  = 'scale3d(' + scale + ', ' + scale + ', ' + scale + ')';
-		var rotateTransform = 'rotateX(-90deg) rotateY(5deg) rotateZ(1deg)';
+		setTimeout(function () {
+			$body.bind(selectionEvent, zoomToHotspot);
+		}, 1);
 
+		var $spread = $spot.parents('.spread');
+		var scale = (1 - ((1 - curSceneScale) * 0.3));
+		var scaleTransform = 'scale3d(' + scale + ', ' + scale + ', ' + scale + ')';
 		var translateTransform;
+		var spreadTransform;
 		if ($spread.hasClass('intro')) {
-			translateTransform = 'translate3d(400px, -200px, 400px)';
+			translateTransform = 'translateY(-200px) translateX(400px) translateZ(400px) rotateX(-90deg) rotateY(5deg) rotateZ(1deg)';
 		} else if ($spread.hasClass('location')) {
-			translateTransform = 'translate3d(-330px, -150px, 400px)';
+			translateTransform = 'translateY(-150px) translateX(-330px) translateZ(400px) rotateX(-90deg) rotateY(5deg) rotateZ(1deg)';
 		} else if ($spread.hasClass('music')) {
-			translateTransform = 'translate3d(315px, -200px, 400px)';
+			translateTransform = 'translateY(-200px) translateX(315px) translateZ(400px) rotateX(-90deg) rotateY(5deg) rotateZ(1deg)';
 		}
-
-		var spreadTransform = [scaleTransform, translateTransform, rotateTransform].join(' ');
+		spreadTransform = scaleTransform + ' ' + translateTransform;
 		$scene.css(cssTransformProperty, spreadTransform);
 	}
+
+	zoomedIn = !zoomedIn;
 }
 
 
@@ -357,21 +389,16 @@ function onHotspotSelected(e) {
 
 
 function rotateScene(e) {
-	if (zoomedIn) return;
-	var o     = e.originalEvent;
+	var o = e.originalEvent;
 	var theta = (Math.abs(window.orientation) == 90) ? o.beta : o.gamma;
-	curRotY   = e.type === 'deviceorientation' ? 0 + (15 * (theta / -45)) : -15 + (30 * e.pageX / $body.width());
-	curRotX   = -15;
+	curRotY = e.type === 'deviceorientation' ? 0 + (15 * (theta / -45)) : -15 + (30 * e.pageX / $body.width());
+	curRotX = -15;
 
 	adjustScene();
 }
 
 function adjustScene() {
-	var scaleTransform     = 'scale3d(' + curSceneScale + ', ' + curSceneScale + ', ' + curSceneScale + ')';
-	var rotateTransform    = 'rotateX(' + curRotX + 'deg) rotateY(' + curRotY + 'deg)';
-	var translateTransform = 'translateY(100px)';
-	var sceneTransform     =  [scaleTransform, rotateTransform, translateTransform].join(' ');
-
+	var sceneTransform = 'scale3d(' + curSceneScale + ', ' + curSceneScale + ', ' + curSceneScale + ') translateY(100px) rotateX(' + curRotX + 'deg) rotateY(' + curRotY + 'deg)';
 	$scene.css(cssTransformProperty, sceneTransform);
 }
 
@@ -442,21 +469,24 @@ function clamp( val, min, max ){
 
 
 function craftThatPaperBaby() {
+	popups = [];
 	$('.popup').each(function (i) {
 		var $popup = $(this);
 		var master = $popup.parents('.spread');
-		var depth  = $popup.attr('data-depth');
-		var popup  = new Popup($popup, depth, foldingEvent);
+		var depth = $popup.attr('data-depth');
+		var popup = new Popup($popup, depth, paperFolding);
+
+		popups.push(popup);
 	});
 
 	$spreads.each(function (i) {
-		var anglePerPage  = 180 / 9;
-		var offsetIndex   = 5 + curPageIndex - i;
-		var offsetAngle   = offsetIndex - 1;
-		var rads          = degToRad(offsetAngle * anglePerPage + 10);
-		var tarX          = 5 * Math.cos(rads);
-		var tarZ          = 5 * Math.sin(rads);
-		var $spread       = $(this);
+		var anglePerPage = 180 / 9;
+		var offsetIndex = 5 + curPageIndex - i;
+		var offsetAngle = offsetIndex - 1;
+		var rads = degToRad(offsetAngle * anglePerPage + 10);
+		var tarX = 5 * Math.cos(rads);
+		var tarZ = 5 * Math.sin(rads);
+		var $spread = $(this);
 		var pageTransform = 'translateX(' + tarX.toFixed(3) + 'px) translateZ(' + tarZ.toFixed(3) + 'px) rotateY(' + (i * 0.5) + 'deg)';
 		$spread.css(cssTransformProperty, pageTransform);
 	});
@@ -473,28 +503,25 @@ function hideLocationBar() {
 
 function Popup(graphic, depth, event) {
 	if (!event) event = 'pageFolding';
-	this.graphic      = graphic;
-	this.POPUP_WIDTH  = 300;
+	this.graphic = graphic;
+	this.depth = depth;
+	this.zRot = graphic.parents('.page').hasClass('page-left') ? 15 : -15;
+	this.POPUP_WIDTH = 300;
 	this.POPUP_SQUARE = this.POPUP_WIDTH * this.POPUP_WIDTH;
-	this.pwsr         = this.POPUP_WIDTH * Math.sin(degToRad(-15));
-	this.pwsrs        = this.pwsr * this.pwsr;
-	this.transforms   = {};
+	this.pwsr = this.POPUP_WIDTH * Math.sin(degToRad(-15));
+	this.pwsrs = this.pwsr * this.pwsr
+	;
 
 	var onFold = function (e, per) { this.setFold(per); }.bind(this);
-	graphic.parents('.spread').bind(foldingEvent, onFold);
-
-	var zRot = graphic.parents('.page').hasClass('page-left') ? 15 : -15;
-	this.transforms.tY = 'translateY(' + depth + 'px)';
-	this.transforms.rZ = 'rotateZ(' + zRot + 'deg)';
-
+	graphic.parents('.spread').bind(paperFolding, onFold);
 	this.setFold(1);
 }
 
 Popup.prototype.setFold = function (fold) {
 	fold = clamp(fold, 0);
-	var adj    = Math.sqrt(this.POPUP_SQUARE - this.pwsrs);
-	var f180   = -180 * fold;
-	var f180r  = degToRad(f180);
+	var adj = Math.sqrt(this.POPUP_SQUARE - this.pwsrs);
+	var f180 = -180 * fold;
+	var f180r = degToRad(f180);
 	var f180nr = degToRad((f180) - 90);
 
 	// origin
@@ -511,7 +538,7 @@ Popup.prototype.setFold = function (fold) {
 
 
 	// normalize the vectors
-	var len    = Math.sqrt(p1[0] * p1[0] + p1[1] * p1[1] + p1[2] * p1[2]);
+	var len = Math.sqrt(p1[0] * p1[0] + p1[1] * p1[1] + p1[2] * p1[2]);
 	var normV1 = $V([p1[0] / len, p1[1] / len, p1[2] / len]);
 	var normV2 = $V([p2[0] / len, p2[1] / len, p2[2] / len]);
 	var normV3 = $V([p3[0] / len, p3[1] / len, p3[2] / len]);
@@ -523,7 +550,6 @@ Popup.prototype.setFold = function (fold) {
 	var crossAngle = -radToDeg(cross.angleFrom(normV3)) - 90;
 
 	// transform the shape
-	var rX = 'rotateX(' + crossAngle + 'deg)';
-	var transform = [this.transforms.tY, this.transforms.rZ, rX].join(' ');
+	var transform = 'translateY(' + this.depth + 'px) rotateZ(' + this.zRot + 'deg) rotateX(' + crossAngle + 'deg)';
 	this.graphic.css(cssTransformProperty, transform);
-};
+}
